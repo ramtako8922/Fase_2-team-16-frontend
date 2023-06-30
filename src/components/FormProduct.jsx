5904ebc055dea22c430480b907261b7e15a69c4d
Ricardo <rmc892020@gmail.com>
2023-06-30 03:45:55 -0500

    fix: addd product form with validations

--- src/components/FormProduct.jsx
+++ src/components/FormProduct.jsx
@@ -7,72 +7,91 @@ import Logo from '../../public/logo.png';
 import Image from 'next/image';
 import useFormProduct from '@/hooks/useFormProduct';
 
+
+
 const FormProduct = () => {
-	const { handlerChange, handlerSubmit } = useFormProduct();
+	const { handlerChange, handlerSubmit,handleSubmit,errors, register } = useFormProduct();
 
 	return (
-		<div className=' min-h-screen flex items-center justify-center p-4 flex-col text-white'>
-			<div className='bg-secondary-100 p-8 rounded-xl  w-auto lg:w-[450px]'>
+		<div className=' h-full  mt-10 flex items-center justify-center p-4 flex-col text-white'>
+			<div className='bg-secondary-100 p-8 rounded-xl  w-30 shadow-2xl lg:w-[450px]'>
 				<h1 className='text-3xl text-center uppercase font-bold tracking-[5px] text-black mb-8'>
 					New <span className='text-primary'>Product</span>
 				</h1>
 				<form
 					className='mb-8'
-					onSubmit={handlerSubmit}>
+					onSubmit={handleSubmit(handlerSubmit)}>
 					<div className='relative mb-4'>
 						<BsFillBoxFill className='absolute top-1/2 -translate-y-1/2 left-2 text-white' />
 						<input
 							type='text'
-							className='py-3 pl-8 pr-4 bg-input_auth w-full focus:bg-input_auth outline-none rounded-lg'
+							className='py-3 pl-8 pr-4 bg-input_auth w-80 focus:bg-input_auth outline-none rounded-lg'
 							placeholder='Name product'
 							name='nameproduct'
-							onChange={handlerChange}
+							{...register('nameproduct')}
 						/>
+						<p className='absolute w-full top-1/2 text-sm translate-y-[90%] left-2  text-error mt-2 mb-2 '>
+								{errors.nameproduct?.message}
+							</p>
 					</div>
 					<div className='relative mb-4'>
 						<MdDescription className='absolute top-1/2 -translate-y-1/2 left-2 text-white' />
 						<input
 							type='text'
-							className='py-3 pl-8 pr-4 bg-input_auth w-full focus:bg-input_auth outline-none rounded-lg'
+							className='py-3 pl-8 pr-4 bg-input_auth w-80 focus:bg-input_auth outline-none rounded-lg'
 							placeholder='Description'
 							name='description'
-							onChange={handlerChange}
+							{...register('description')}
 						/>
+						<p className='absolute w-full top-1/2 text-sm translate-y-[90%] left-2  text-error mt-2 mb-2 '>
+								{errors.description?.message}
+							</p>
 					</div>
-					{/* <div className='relative mb-4'>
+					<div className='relative mb-4'>
 						<MdConfirmationNumber className='absolute top-1/2 -translate-y-1/2 left-2 text-white' />
-						<select name='category'
-						        onChange={handlerChange}
-							className='py-3 pl-8 pr-4 bg-input_auth w-full focus:bg-input_auth outline-none rounded-lg text-gray-400'>
+						<select name='stoock'
+						        {...register('stoock')}
+							className='py-3 pl-8 pr-4 bg-input_auth w-80 focus:bg-input_auth outline-none rounded-lg text-gray-400'>
 							<option value='' selected>Stoock State</option>
 							<option value='si'>Yes</option>
 							<option value='no'>No</option>
 							
 						</select>
-					</div> */}
+						<p className='absolute w-full top-1/2 text-sm translate-y-[90%] left-2  text-error mt-2 mb-2 '>
+								{errors.stoock?.message}
+							</p>
+					</div>
 					<div className='relative mb-4'>
 						<HiCurrencyDollar className='absolute top-1/2 -translate-y-1/2 left-2 text-white' />
 						<input
-							className='py-3 pl-8 pr-4 bg-input_auth w-full focus:bg-input_auth outline-none rounded-lg'
+							className='py-3 pl-8 pr-4 bg-input_auth w-80 focus:bg-input_auth outline-none rounded-lg'
 							placeholder='Price'
 							name='price'
-							onChange={handlerChange}
+							{...register('price')}
 						/>
+						<p className='absolute w-full top-1/2 text-sm translate-y-[90%] left-2  text-error mt-2 mb-2 '>
+								{errors.price?.message}
+							</p>
 					</div>
 					<div className='relative mb-4'>
 						<RiBarChart2Fill className='absolute top-1/2 -translate-y-1/2 left-2 text-white' />
 						<input
-							className='py-3 pl-8 pr-4 bg-input_auth w-full focus:bg-input_auth outline-none rounded-lg'
+							className='py-3 pl-8 pr-4 bg-input_auth w-80 focus:bg-input_auth outline-none rounded-lg'
 							placeholder=' product quantity'
 							name='quantity'
-							onChange={handlerChange}
+							{...register('quantity')}
+							
+							
 						/>
+						<p className='absolute w-full top-1/2 text-sm translate-y-[90%] left-2  text-error mt-2 mb-2 '>
+								{errors.price?.message}
+							</p>
 					</div>
-					{/* <div className='relative mb-8'>
+					<div className='relative mb-8'>
 						<RiArchiveDrawerFill className='absolute top-1/2 -translate-y-1/2 left-2 text-white' />
 						<select name='category'
-						     onChange={handlerChange}
-							className='py-3 pl-8 pr-4 bg-input_auth w-full focus:bg-input_auth outline-none rounded-lg text-gray-400'>
+						     {...register('category')}
+							className='py-3 pl-8 pr-4 bg-input_auth w-80 focus:bg-input_auth outline-none rounded-lg text-gray-400'>
 							<option value='' selected>Select Category</option>
 							<option value='volvo'>Game</option>
 							<option value='saab'>Toys</option>
@@ -82,20 +101,19 @@ const FormProduct = () => {
 							<option value='opel'>Opel</option>
 							<option value='audi'>Audi</option>
 						</select>
-					</div> */}
+						<p className='absolute w-full top-1/2 text-sm translate-y-[90%] left-2  text-error mt-2 mb-2 '>
+								{errors.category?.message}
+							</p>
+					</div>
 					<div>
 						<button
 							type='submit'
-							className='bg-primary text-black uppercase font-bold text-sm w-full py-3 px-4 rounded-lg'>
+							className='bg-input_auth text-primary uppercase font-bold text-sm w-80 py-3 px-4 rounded-lg'>
 							Register
 						</button>
 					</div>
 				</form>
-				<Image
-					src={Logo}
-					alt='Logo ZurmC'
-					className='mt-8 translate-y-20'
-				/>
+				
 			</div>
 		</div>
 	);
--- src/hooks/useFormProduct.jsx
+++ src/hooks/useFormProduct.jsx
@@ -7,9 +7,31 @@ import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { useCreateProductMutation } from '@/store/slices/apis';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { ProductSchema } from '@/validations/ValidationProductForm';
+import { yupResolver } from '@hookform/resolvers/yup';
+import *as youp from 'yup';
 
 function useFormProduct() {
-	const [form, setForm] = useState(initialState);
+
+	const {
+		register,
+		reset,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			productname: '',
+			description: '',
+			stoock: '',
+			price: '',
+			quantity: '',
+			category:''
+		},
+		resolver: yupResolver(ProductSchema),
+	});
+
+	const [form,setForm]=useState(initialState)
 	const dispatch = useDispatch();
 	const [createProduct] = useCreateProductMutation();
 
@@ -20,26 +42,33 @@ function useFormProduct() {
 		});
 	};
 
-	const handlerSubmit = (e) => {
+	const handlerSubmit= async (e) => {
 		e.preventDefault();
+		console.log("e",e)
 
-		const { nameproduct, description, price, quantity } = e.target;
-
+		const { nameproduct, description,stoock, price, quantity,category } = e.target;
+		console.log("e",e)
 		const product = {
 			id: uuid(),
 			name: nameproduct.value,
 			description: description.value,
+            stoock:stoock.value,
 			price: price.value,
 			quantity: quantity.value,
+            category:category.value 
 		};
-
+         console.log(product)
 		createProduct(product);
+		reset()
 	};
 
 	return {
 		handlerChange,
 		handlerSubmit,
 		createProduct,
+		errors,
+		register,
+		handleSubmit
 	};
 }
 
