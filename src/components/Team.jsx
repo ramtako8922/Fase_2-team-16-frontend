f1c8ceffe94a00730a0c876de7256348aa3f9868
Ricardo <rmc892020@gmail.com>
2023-07-28 23:25:14 -0500

    page team

---
+++ src/components/Team.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import Image from 'next/image'
+import picture from '../../public/oig.jpg'
+import picture2 from '../../public/rm.jpg'
+import picture3 from '../../public/av.jpg'
+import picture4 from '../../public/fa.jpg'
+import picture5 from '../../public/ff.jpg'
+import picture6 from '../../public/jr.jpg'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import {
+  RiLinkedinFill,
+  RiGithubFill
+} from 'react-icons/ri';
+
+export default function Team() {
+  return (
+    <>
+    <div className='w-[800px]'>
+      <h1 className=' font-medium text-center mt-10 text-2xl'><span className='text-2xl text-purple-500'>Our</span>Team</h1>
+      <h2 className='text-center text-2xl mt-4  font-medium '>Team Lead</h2>
+      <div className='flex-col ml-8 p-4 md:flex'>
+        <motion.div className=' shadow-xl bg-slate-300 flex mt-4 gap-4 '  initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:1}}>
+          <div className='shrink-0' >
+            <Image className='h-full' src={picture4}
+              width={250} />
+
+          </div>
+          <div className='w-1/4 flex-col' >
+            <p className='text-purple-500'>Franklin Andres Rodriguez Gonzalez</p>
+            <h4 className='font-medium'>Nacionalidad: <span>Colombia</span></h4>
+            <h4 className='font-medium'>Perfil</h4>
+            <p className=''>Fundador de TechLeap, Scrum Master en Try Catch.tv</p>
+            <h4 className='font-medium'>Redes sociale</h4>
+            <div className='inline-flex gap-2'>
+              <Link href="https://www.linkedin.com/in/franklin-andres-rodriguez-gonzalez-7b7663272/"><RiLinkedinFill className='text-purple-500 text-2xl' /></Link>
+              <Link href="https://github.com/FranklinAndresNegusNet"><RiGithubFill className='text-purple-500 text-2xl' /></Link>
+            </div>
+            </div>
+          </motion.div>
+          </div>
+        </div>
+    
+
+    <h2 className='text-center text-2xl mt-8  font-medium '>Backend Team</h2><div className='flex-col ml-8 gap-2 p-4 md:flex-col sm:flex-col xl:flex-row '>
+        
+        <motion.div className=' shadow-2xl bg-slate-300 flex mt-4  w-1/3   gap-2'  initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:1}}>
+          <div className='shrink-0 '>
+            <Image className='h-full' src={picture5}
+              width={250} />
+          </div>
+          <div className='w-1/2 flex-col'>
+            <p className='text-purple-500'>Fabrizio Ferroni </p>
+            <h4 className='font-medium'>Nacionalidad: <span>Argentina</span></h4>
+            <h4 className='font-medium'>Perfil</h4>
+            <p className=''>Desarrollador web full stack JR</p>
+            <h4 className='font-medium'>Redes sociales</h4>
+            <div className='inline-flex gap-2'>
+              <Link href="https://www.linkedin.com/in/fabrizio-ferroni/"><RiLinkedinFill className='text-purple-500 text-2xl' /></Link>
+              <Link href="https://github.com/FabrizioFerroni"><RiGithubFill className='text-purple-500 text-2xl' /></Link>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div className='shadow-xl bg-slate-300 flex mt-4  w-1/3   gap-2' initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:1}}>
+          <div className='shrink-0 '>
+            <Image className='object-cover h-full ' src={picture6}
+              width={250} />
+          </div>
+          <div className=' flex-col'>
+            <p className='text-purple-500'>Julio Román </p>
+            <h4 className='font-medium'>Nacionalidad: <span>Perú</span></h4>
+            <h4 className='font-medium'>Perfil</h4>
+            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h4 className='font-medium'>Redes sociales</h4>
+            <div className='inline-flex gap-2'>
+              <Link href="https://github.com/jromanz"><RiGithubFill className='text-purple-500 text-2xl' /></Link>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div className=' shadow-2xl bg-slate-300 flex mt-4  w-1/3   gap-2' initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:1}}>
+          <div className='shrink-0 '>
+            <Image className='object-cover h-full'  src={picture}
+             width={250} />
+          </div>
+          <div className='w-1/4 flex-col'>
+            <p className='text-purple-500'>Kevin JH </p>
+            <h4 className='font-medium'>Nacionalidad: <span>Colombia</span></h4>
+            <h4 className='font-medium'>Perfil</h4>
+            <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h4 className='font-medium'>Redes sociales</h4>
+            <div className='inline-flex gap-2'>
+              <Link href="#"><RiLinkedinFill className='text-purple-500 text-2xl' /></Link>
+              <Link href="#"><RiGithubFill className='text-purple-500 text-2xl' /></Link>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+      
+      <h2 className='text-center text-2xl mt-8 font-medium '>Frontend Team</h2><div className='flex-col ml-8  p-4 md:flex-col xl:flex-row'>
+
+
+        <motion.div className=' shadow-2xl bg-slate-300 flex mt-4  w-1/2   gap-2' initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:1}}>
+          <div className='shrink-0'>
+            <Image className=' object-cover h-full'  src={picture3}
+              width={250} />
+          </div>
+          <div className='w-1/4 flex-col'>
+            <p className='text-purple-500'>Andres Valencia </p>
+            <h4 className='font-medium'>Nacionalidad: <span>Mexico</span></h4>
+            <h4 className='font-medium'>Perfil</h4>
+            <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h4 className='font-medium'>Redes sociales</h4>
+            <div className='inline-flex gap-2'>
+              <Link href="https://www.linkedin.com/in/andres-valencia-8b792b103/"><RiLinkedinFill className='text-purple-500 text-2xl' /></Link>
+              <Link href="https://github.com/sirandresitog"><RiGithubFill className='text-purple-500 text-2xl' /></Link>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div className=' shadow-2xl bg-slate-300 flex mt-4  w-1/2   gap-2'  initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:1}}>
+          <div>
+            <Image className='shrink-0 h-full' src={picture2}
+              width={250} />
+          </div>
+          <div className='w-1/4 flex-col ml-2'>
+            <p className='text-purple-500'>Ricardo Mejía </p>
+            <h4 className='font-medium'>Nacionalidad: <span>Colombia</span></h4>
+            <h4 className='font-medium'>Perfil</h4>
+            <p className=''>JR Full-Stack Developer,REACT JS, JAVA, C#, VUE JS</p>
+            <h4 className='font-medium'>Redes sociales</h4>
+            <div className='inline-flex gap-2'>
+              <Link href="https://www.linkedin.com/in/ricardo-andres-mejia-cordoba/"><RiLinkedinFill className='text-purple-500 text-2xl' /></Link>
+              <Link href="https://github.com/ramtako8922"><RiGithubFill className='text-purple-500 text-2xl' /></Link>
+            </div>
+          </div>
+        </motion.div>
+
+      </div>
+    
+    </>
+
+
+
+
+
+
+
+  )
+}
+
+
---
+++ src/pages/aboutus/Team.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Team from '@/components/Team'
+function team() {
+  return (
+    <div>
+    
+     <div>
+     <Team/>
+     </div>
+
+   
+   
+ </div>
+  )
+}
+
+export default team
